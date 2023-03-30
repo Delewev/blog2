e23c904b6b926dd6f6653e04eaf52f4497ac6286
Damir <delewev.damir@mail.ru>
2023-03-30 22:24:20 +0300

    categories and tags

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Main'], function () {
+Route::get('/', 'IndexController');
 });
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function (){
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'IndexController')->name('admin.categories.index');
+        Route::get('/create', 'CreateController')->name('admin.categories.create');
+        Route::post('/', 'StoreController')->name('admin.category.store');
+        Route::get('/{category}', 'ShowController')->name('admin.category.show');
+        Route::get('/{category}/edit', 'EditController')->name('admin.category.edit');
+        Route::patch('/{category}', 'UpdateController')->name('admin.category.update');
+        Route::delete('/{category}', 'DeleteController')->name('admin.category.delete');
+    });
+    Route::group(['namespace' => 'Tag', 'prefix' => 'tags'], function () {
+        Route::get('/', 'IndexController')->name('admin.tags.index');
+        Route::get('/create', 'CreateController')->name('admin.tags.create');
+        Route::post('/', 'StoreController')->name('admin.tag.store');
+        Route::get('/{tag}', 'ShowController')->name('admin.tag.show');
+        Route::get('/{tag}/edit', 'EditController')->name('admin.tag.edit');
+        Route::patch('/{tag}', 'UpdateController')->name('admin.tag.update');
+        Route::delete('/{tag}', 'DeleteController')->name('admin.tag.delete');
+    });
+});
+
+Auth::routes();
+
+
